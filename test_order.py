8e1addc72008b90e0dfe2b74eb4e99a36637ef84
Magwit <magnushakon@hotmail.com>
2020-03-17 10:33:10 +0100

    Write Given - When - Then statement for test_order

--- test_order.py
+++ test_order.py
@@ -2,9 +2,14 @@ import pytest
 
 
 def test_placing_order(browser):
+    # Given The user is on Red pepper Pizza
     URL = "https://magwit.github.io/pizza_site/"
     browser.get(URL)
-    browser.implicitly_wait(10)
-    # yield browser
-    assert 2 == 2
-    browser.quit()
+
+    # When The user clicks on the Add to order button below "Opera"
+    # And The user clicks on the Add to otrder button below "Frutti di Mare"
+
+    # Then Opera and Frutti di Mare appears as order items in Your order
+    # And the sum of the order is the correct amount 13.50
+
+    pass
