711a6975acf8a019d83a9199c96e310fc9a7c264
Magwit <magnushakon@hotmail.com>
2020-03-24 16:07:19 +0100

    get test_order ready for gif recoding

--- test_order.py
+++ test_order.py
@@ -1,56 +1,61 @@
-import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+# TODO record gifs and then remove sleep
+# TODO screenshots on error
+
 
 def test_placing_order(browser):
-    # Given The user is on Red pepper Pizza
+    # GIVEN The user is on Red pepper Pizza
     URL = "https://magwit.github.io/pizza_site/"
     browser.get(URL)
 
     opera_order_xpath = "//form[@id='oper']/button"
     frutti_order_xpath = "//form[@id='frut']/button"
-    order_items_xpath = "//*[@class='order-items']"
-    order_items = browser.find_element(By.XPATH, order_items_xpath)
 
     wait = WebDriverWait(browser, 20)
 
     try:
-        # wait.until(
-        #     EC.presence_of_all_elements_located((opera_order_btn, frutti_order_btn))
-        # )
         wait.until(EC.element_to_be_clickable((By.XPATH, opera_order_xpath)))
     except Exception as e:
+        print(type(e))
         print(e.args)
 
-    # When The user clicks on the Add to order button below "Opera"
+    # WHEN The user clicks on the Add to order button below "Opera"
+
     opera_order_btn = browser.find_element(By.XPATH, opera_order_xpath)
     opera_order_btn.click()
 
-    # And The user clicks on the Add to order button below "Frutti di Mare"
+    # AND The user clicks on the Add to order button below "Frutti di Mare"
 
     frutti_order_btn = browser.find_element(By.XPATH, frutti_order_xpath)
     frutti_order_btn.click()
 
-    # Then Opera and Frutti di Mare appears as order items in Your order
+    # THEN Opera and Frutti di Mare appears as order items in Your order
+
+    try:
+        # wait.until(browser.find_elements(By.XPATH, "//*[@class='order-items']/*"))
+        wait.until(
+            EC.presence_of_element_located((By.XPATH, "//*[@class='order-items']/*"))
+        )
+    except Exception as e:
+        print(type(e))
+        print(e.args)
 
     # xpath for the two order divs for Opera and Frutti di Mare
     div_xpath = "//*[@class='order-items']/div[@id='oper-order' or @id='frut-order']"
 
-    order_content = browser.find_elements_by_xpath(div_xpath)
+    order_content = browser.find_elements(By.XPATH, div_xpath)
 
-    no_order_items = len(order_content)
+    # assert that the two specific prder items are nested in the order-items div
+    assert len(order_content) == 2
 
-    assert no_order_items == 2
-
-    # And the sum of the order is the correct amount 13.50
+    # AND the sum of the order is the correct amount 13.50
 
     sum_input_xpath = "//input[@name='total']"
-
     sum_input = browser.find_element(By.XPATH, sum_input_xpath).get_property("value")
-
     assert sum_input == "13.50"
 
     time.sleep(2)
