c2b0d193ffa2b8857ca9c0ad4b8b79c7b5744af1
Magwit <magnushakon@hotmail.com>
2020-03-25 15:56:40 +0100

    test search ready for gif recording

---
+++ test_search.py
@@ -0,0 +1,50 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# TODO record gifs and then remove sleep
+# TODO screenshots on error
+
+
+def test_search_menu_item(browser):
+
+    # GIVEN The user is on Red pepper Pizza
+
+    URL = "https://magwit.github.io/pizza_site/"
+    browser.get(URL)
+
+    wait = WebDriverWait(browser, 20)
+
+    # WHEN The user searches for olives
+
+    try:
+        search_bar = browser.find_element(By.ID, "pizza-search")
+        search_bar.send_keys("olives")
+    except Exception as e:
+        print(type(e))
+        print(e.args)
+
+    # THEN the menu is filtered to display only Opera and Frutti di Mare
+    # OR the menu is filtered to display only Opera, Frutti di Mare and Boss' surprise
+
+    # wait for elements to be hidden
+    try:
+        wait.until_not(
+            EC.visibility_of_any_elements_located(
+                (By.XPATH, "//formm[@class='itemform']")
+            )
+        )
+    except Exception as e:
+        print(type(e))
+        print(e.arg)
+
+    display_matches_xath = "//form[@class='itemForm'][@style!='display: none;']"
+    ingredient_matches_xpath = "//form/p[contains(text(), 'olives')]"
+
+    display_matches = browser.find_elements(By.XPATH, display_matches_xath)
+    ingredient_matches = browser.find_elements(By.XPATH, ingredient_matches_xpath)
+
+    assert len(display_matches) == len(ingredient_matches)
+
+    time.sleep(2)
